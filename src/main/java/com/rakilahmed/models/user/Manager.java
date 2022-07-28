1463065f9be36bb3d8ec4ffede4046e9fe9eacff
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-07-27 22:29:33 -0400

    Modified the user models

--- src/main/java/com/rakilahmed/models/user/Manager.java
+++ src/main/java/com/rakilahmed/models/user/Manager.java
@@ -1,66 +1,22 @@
 package com.rakilahmed.models.user;
 
 public class Manager extends User {
-    private String managerName;
-    private String email;
-
     /**
      * Default constructor for Manager class.
      */
     public Manager() {
         super();
-        this.managerName = "";
-        this.email = "";
     }
 
     /**
      * Parameterized constructor for Manager class.
      * 
-     * @param userId
      * @param username
      * @param password
      * @param managerName
      * @param email
      */
-    public Manager(int userId, String username, String password, String managerName, String email) {
-        super(userId, username, password);
-        this.managerName = managerName;
-        this.email = email;
-    }
-
-    /**
-     * Getter for managerName.
-     * 
-     * @return managerName
-     */
-    public String getManagerName() {
-        return managerName;
-    }
-
-    /**
-     * Getter for email.
-     * 
-     * @return email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Setter for managerName.
-     * 
-     * @param managerName
-     */
-    public void setManagerName(String managerName) {
-        this.managerName = managerName;
-    }
-
-    /**
-     * Setter for email.
-     * 
-     * @param email
-     */
-    public void setEmail(String email) {
-        this.email = email;
+    public Manager(String username, String password, String managerName, String email) {
+        super(username, password, managerName, email);
     }
 }
--- src/main/java/com/rakilahmed/models/user/User.java
+++ src/main/java/com/rakilahmed/models/user/User.java
@@ -4,6 +4,8 @@ public abstract class User {
     private int userId;
     private String username;
     private String password;
+    private String fullName;
+    private String email;
 
     /**
      * Default constructor for User class.
@@ -12,19 +14,23 @@ public abstract class User {
         this.userId = 0;
         this.username = "";
         this.password = "";
+        this.fullName = "";
+        this.email = "";
     }
 
     /**
      * Parameterized constructor for User class.
      * 
-     * @param userId
      * @param username
      * @param password
+     * @param fullName
+     * @param email
      */
-    public User(int userId, String username, String password) {
-        this.userId = userId;
+    public User(String username, String password, String fullName, String email) {
         this.username = username;
         this.password = password;
+        this.fullName = fullName;
+        this.email = email;
     }
 
     /**
@@ -55,6 +61,24 @@ public abstract class User {
     }
 
     /**
+     * Getter for fullName.
+     * 
+     * @return fullName
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * Getter for email.
+     * 
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
      * Setter for userId.
      * 
      * @param userId
@@ -79,5 +103,23 @@ public abstract class User {
      */
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    /**
+     * Setter for fullName.
+     * 
+     * @param fullName
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * Setter for email.
+     * 
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
