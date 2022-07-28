f7e95561d7d8a7f85f8a03327c3e5a1948fcff16
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-07-27 22:32:16 -0400

    ConnectionManager added and logger setup

---
+++ src/main/java/com/rakilahmed/utils/ConnectionManager.java
@@ -0,0 +1,78 @@
+package com.rakilahmed.utils;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private final String url;
+    private final String username;
+    private final String password;
+    private final Driver driver;
+    private final boolean driverRegistered;
+
+    /**
+     * Default constructor for ConnectionManager class.
+     */
+    public ConnectionManager() {
+        this.url = "jdbc:postgresql://revature-db.cn6l9mwmtabj.us-east-1.rds.amazonaws.com:5432/ers";
+        this.username = "postgres";
+        this.password = "1121Revature";
+        this.driver = new org.postgresql.Driver();
+        this.driverRegistered = false;
+    }
+
+    /**
+     * Parameterized constructor for ConnectionManager class.
+     */
+    public ConnectionManager(String url, String username, String password, Driver driver) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.driver = driver;
+        this.driverRegistered = false;
+    }
+
+    /**
+     * Registers the driver.
+     *
+     * @throws SQLException
+     */
+    private void registerDriver() throws SQLException {
+        if (!driverRegistered) {
+            DriverManager.registerDriver(this.driver);
+        }
+    }
+
+    /**
+     * Creates a connection.
+     *
+     * @return the connection
+     * @throws SQLException
+     */
+    public Connection getConnection() throws SQLException {
+        if (!driverRegistered) {
+            this.registerDriver();
+        }
+
+        try {
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new SQLException("Failed to get connection: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Closes the connection.
+     *
+     * @param connection the connection
+     */
+    public void close() {
+        try {
+            this.getConnection().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
